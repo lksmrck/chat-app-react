b44ecd9da90de59acd2c548f2a518d8afcb5c5d4
LukasMercak <lukasthevicious@gmail.com>
2023-03-20 20:37:19 +0100

    setup, styled components, theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import MainPage from "./pages/MainPage";
+import { useState } from "react";
+import LoginPage from "./pages/LoginPage";
+import Layout from "./components/ui/Layout";
+import { Routes as RouterRoutes, Route, Navigate } from "react-router-dom";
+import ProtectedRoutes from "./components/login/ProtectedRoutes";
 
 function App() {
   return (
     <div>
-      <MainPage />
+      <Layout>
+        <RouterRoutes>
+          <Route path="/" element={<Navigate replace to="/login" />} />
+          {/*     <Route path="*" element={<NotFound />} /> */}
+          <Route path="/login" element={<LoginPage />} />
+          {/*     <Route element={<ProtectedRoutes allowedRights={["tbd"]} />}> */}
+          <Route path="/conversations" element={<MainPage />} />
+          {/*   </Route> */}
+        </RouterRoutes>
+      </Layout>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,22 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./common/theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <ChakraProvider>
-    <App />
+    <BrowserRouter>
+      <AuthContextProvider>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </AuthContextProvider>
+    </BrowserRouter>
   </ChakraProvider>
 );
