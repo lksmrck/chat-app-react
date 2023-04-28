1b8194af807607f51cfceb4095e75b0be8b61502
LukasMercak <lukasthevicious@gmail.com>
2023-04-29 00:56:21 +0200

    final touches

--- server/index.js
+++ server/index.js
@@ -14,7 +14,8 @@ dotenv.config();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
+    methods: ["GET", "POST", "DELETE"],
   })
 );
 
@@ -28,7 +29,7 @@ const server = http.createServer(app);
 
 export const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", //FE
+    origin: "*", //FE
     methods: ["GET", "POST"],
   },
 });
