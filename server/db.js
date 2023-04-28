7ab64a326bc9fd5be598c83c5940d2fe04856e47
LukasMercak <lukasthevicious@gmail.com>
2023-04-29 00:23:04 +0200

    final touches

--- server/db.js
+++ server/db.js
@@ -10,7 +10,7 @@ const pool = new Pool({
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   host: process.env.POSTGRES_HOST,
-  port: POSTGRES_PORT,
+  port: process.env.POSTGRES_PORT,
   database: process.env.POSTGRES_DB,
 });
 
